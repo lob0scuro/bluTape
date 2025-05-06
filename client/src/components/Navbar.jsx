0b3793e35c869c73dfd898952ed0e68af75082ea
Cameron <kamrin717@gmail.com>
2025-05-06 17:09:20 -0500

    made navbar openable

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,37 @@
 import styles from "../style/Navbar.module.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faEllipsis, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const { user } = useAuth();
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <Link to="/">
         <h1>bluTape/{user && <small>{user.first_name}</small>}</h1>
       </Link>
+      <FontAwesomeIcon
+        icon={faBars}
+        className={styles.hamburger}
+        style={navOpen ? { color: "#8999af" } : { color: "inherit" }}
+        onClick={() => setNavOpen(!navOpen)}
+      />
       <nav>
         {user && (
-          <div className={styles.navLinks}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/start">Start</NavLink>
-            <NavLink to="/active">Active</NavLink>
-            <NavLink to="/finished">Finished</NavLink>
-          </div>
+          <>
+            {navOpen && (
+              <div className={styles.navLinks}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/start">Start</NavLink>
+                <NavLink to="/active">Active</NavLink>
+                <NavLink to="/finished">Finished</NavLink>
+              </div>
+            )}
+          </>
         )}
       </nav>
     </header>
