82acd2ffe91c5b1ac4568ea23e340630befa0121
Cameron <kamrin717@gmail.com>
2025-05-06 18:30:57 -0500

    changed navbar behavior on close

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import styles from "../style/Navbar.module.css";
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faEllipsis, faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -8,10 +8,15 @@ import { faBars, faEllipsis, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   const { user } = useAuth();
   const [navOpen, setNavOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setNavOpen(false);
+  }, [location.pathname]);
 
   return (
     <header className={styles.header}>
-      <Link to="/">
+      <Link to="/" onClick={() => setNavOpen(false)}>
         <h1>bluTape/{user && <small>{user.first_name}</small>}</h1>
       </Link>
       <FontAwesomeIcon
@@ -25,10 +30,18 @@ const Navbar = () => {
           <>
             {navOpen && (
               <div className={styles.navLinks}>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/start">Start</NavLink>
-                <NavLink to="/active">Active</NavLink>
-                <NavLink to="/finished">Finished</NavLink>
+                <NavLink onClick={() => setNavOpen(false)} to="/">
+                  Home
+                </NavLink>
+                <NavLink onClick={() => setNavOpen(false)} to="/start">
+                  Start
+                </NavLink>
+                <NavLink onClick={() => setNavOpen(false)} to="/active">
+                  Active
+                </NavLink>
+                <NavLink onClick={() => setNavOpen(false)} to="/finished">
+                  Finished
+                </NavLink>
               </div>
             )}
           </>
