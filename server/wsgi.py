c0db9189a27fa7fb023312ae4b7b45cc9d2ae636
Cameron Lopez <kamrin717@gmail.com>
2025-07-31 01:11:54 -0500

    pushing to master to test deploy workflow

--- server/wsgi.py
+++ server/wsgi.py
@@ -3,7 +3,8 @@ from app import create_app
 app = create_app()
 
 # Uncomment the following block for development purposes
-
-
+#
+#
+#
 # if __name__ == "__main__":
 #     app.run(port=8000, debug=True)
