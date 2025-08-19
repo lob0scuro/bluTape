37efe2eff83e872bc12898bd55114f02b04dd58f
Cameron <kamrin717@gmail.com>
2025-08-19 09:08:16 -0500

    I think i did it!

--- server/app/labels/routes.py
+++ server/app/labels/routes.py
@@ -2,31 +2,25 @@ from flask import request, jsonify, current_app
 from app.labels import bp
 import socket
 from flask_login import current_user
-import subprocess
 
 #TODO: update with Pi's tailscale IP
 PRINTER_IP = "100.71.48.104"
+ZEBRA_IP = "192.168.1.113"
 PRINTER_PORT = 9100
 
-def send_to_pi(zpl):
-    user = "cameron"
-    subprocess.run(
-        ["ssh", f"{user}@{PRINTER_IP}", f"echo '{zpl}' | sudo tee /dev/usb/lp0 > /dev/null"],
-        check=True,
-    )
 
 def generate_ZPL_label(data):
     zpl = f"""
     ^XA
-    ^FO25,25^GB750,360,6^FS
-    ^FO90, 60^A0,35^FDbluTape/ Matt's Appliances^FS
-    ^FO650, 60^A0,35^FDID: {data["id"]}^FS
-    ^FO90,105^BQN,2,9^FDLA,https://blutape.net/card/{data["id"]}^FS
-    ^FO350,130^A0,35^FDBrand: {data["brand"]}^FS
-    ^FO350,175^A0,35^FDModel: {data["model"]}^FS
-    ^FO350,220^A0,35^FDSerial: {data["serial"]}^FS
-    ^FO350,265^A0,35^FDStyle: {data["style"]}^FS
-    ^FO350,310^A0,35^FDColor: {data["color"]}^FS
+    ^FO10,25^GB700,365,6^FS
+    ^FO50, 60^A0,35^FDbluTape/ Matt's Appliances^FS
+    ^FO530, 60^A0,35^FDID: {data["id"]}^FS
+    ^FO50,105^BQN,2,9^FDLA,https://blutape.net/card/{data["id"]}^FS
+    ^FO300,130^A0,35^FDBrand: {data["brand"]}^FS
+    ^FO300,175^A0,35^FDModel: {data["model"]}^FS
+    ^FO300,220^A0,35^FDSerial: {data["serial"]}^FS
+    ^FO300,265^A0,35^FDStyle: {data["style"]}^FS
+    ^FO300,310^A0,35^FDColor: {data["color"]}^FS
     ^XZ
     """
     return zpl
@@ -55,12 +49,11 @@ def print_label():
         zpl = generate_ZPL_label(data)
         
         #Open socket to Zebra printer via raspberry pi
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #     s.settimeout(5)
-        #     s.connect((PRINTER_IP, PRINTER_PORT))
-        #     s.sendall(zpl.encode("utf-8"))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)
+            s.connect((PRINTER_IP, PRINTER_PORT))
+            s.sendall(zpl.encode("utf-8"))
         
-        send_to_pi(zpl)
         
             
         current_app.logger.info(f"{current_user.first_name} {current_user.last_name} just printed a label")
