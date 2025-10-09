032eba5d04ad71416b8ab7e4d3af5881ce88ffd9
lob0scuro <kamrin717@gmail.com>
2025-10-09 14:05:11 -0500

    finished first part of employee metrics

--- client/src/components/employee-metrics/EmployeeMetrics.jsx
+++ client/src/components/employee-metrics/EmployeeMetrics.jsx
@@ -1,12 +1,22 @@
 import styles from "./EmployeeMetrics.module.css";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { formatDate } from "../../utils/Tools";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faScrewdriverWrench,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import Button from "../buttons/Button";
 
 const EmployeeMetrics = ({ user }) => {
   const today = new Date().toISOString().split("T")[0];
   const [start, setStart] = useState(today);
   const [end, setEnd] = useState(today);
   const [metrics, setMetrics] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserMetrics = async () => {
@@ -27,7 +37,6 @@ const EmployeeMetrics = ({ user }) => {
           if (!data.success) {
             throw new Error(data.message);
           }
-          toast.success("Fetched user metrics.");
           console.log(data.data);
           setMetrics(data.data);
         } else {
@@ -41,6 +50,13 @@ const EmployeeMetrics = ({ user }) => {
     };
     fetchUserMetrics();
   }, [user, start, end]);
+
+  const setDateToToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+    setStart(today);
+    setEnd(today);
+  };
+
   if (!user) {
     return (
       <p style={{ fontSize: "1.2rem", marginTop: "1rem", fontStyle: "italic" }}>
@@ -71,6 +87,94 @@ const EmployeeMetrics = ({ user }) => {
             onChange={(e) => setEnd(e.target.value)}
           />
         </label>
+        <Button
+          onClick={setDateToToday}
+          label="Today"
+          className={styles.todayButton}
+        />
+      </div>
+      <div className={styles.metricResultsContainer}>
+        <h2>
+          Current Workload{" "}
+          <FontAwesomeIcon
+            icon={faScrewdriverWrench}
+            style={{ color: "var(--buttonSecondary)" }}
+          />
+        </h2>
+        <ul className={styles.metrics_list}>
+          {metrics?.in_progress.map((machine) => (
+            <li
+              key={machine.id}
+              onClick={() => navigate(`/card/${machine.id}`)}
+            >
+              {machine.brand.toUpperCase()} - {machine.style} | {machine.serial}
+            </li>
+          ))}
+        </ul>
+        <br />
+        <p>Total: {metrics.in_progress.length}</p>
+      </div>
+
+      <div className={styles.metricResultsContainer}>
+        <h2>
+          Completed between{" "}
+          <FontAwesomeIcon
+            icon={faCircleCheck}
+            style={{ color: "green", fontSize: "1.1rem" }}
+          />{" "}
+          <br />{" "}
+          <span className={styles.dateSpan}>
+            {formatDate(start)} and {formatDate(end)}
+          </span>
+        </h2>
+        <ul className={styles.metrics_list}>
+          {metrics?.completed_in_range.map((machine) => (
+            <li
+              key={machine.id}
+              onClick={() => navigate(`/card/${machine.id}`)}
+            >
+              {machine.brand.toUpperCase()} - {machine.style} | {machine.serial}
+            </li>
+          ))}
+        </ul>
+        <br />
+        <p>Total: {metrics.completed_in_range.length}</p>
+      </div>
+      <div className={styles.metricResultsContainer}>
+        <h2>
+          Trashed between{" "}
+          <FontAwesomeIcon
+            icon={faTrash}
+            style={{ color: "red", fontSize: "1.1rem" }}
+          />
+          <br />{" "}
+          <span className={styles.dateSpan}>
+            {formatDate(start)} and {formatDate(end)}
+          </span>
+        </h2>
+        <ul className={styles.metrics_list}>
+          {metrics?.trashed_in_range.map((machine) => (
+            <li
+              key={machine.id}
+              onClick={() => navigate(`/card/${machine.id}`)}
+            >
+              {machine.brand.toUpperCase()} - {machine.style} | {machine.serial}
+            </li>
+          ))}
+        </ul>
+        <br />
+        <p>Total: {metrics.trashed_in_range.length}</p>
+      </div>
+      <div className={styles.summaryContainer}>
+        <h2>{metrics ? metrics.in_progress.length : 0} </h2>
+        <p>Machines Currently In Progress</p>
+      </div>
+      <div className={styles.summaryContainer}>
+        <h2>{metrics ? metrics.count_completed_trashed : 0} </h2>
+        <p>Machines Worked On Between</p>
+        <p>
+          {formatDate(start)} and {formatDate(end)}
+        </p>
       </div>
     </div>
   );
